5b10f71280bfad4333b7674887d30c47a48a5437
hyun <hyssh@example.com>
2023-10-02 16:50:45 -0700

    add chromadb

--- quickstart-learnfast/document-intelligent/app.py
+++ quickstart-learnfast/document-intelligent/app.py
@@ -6,6 +6,8 @@ To learn more, please visit the documentation - Quickstart: Form Recognizer Pyth
 https://docs.microsoft.com/en-us/azure/applied-ai-services/form-recognizer/quickstarts/try-v3-python-sdk
 """
 
+import os
+from dotenv import load_dotenv
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import DocumentAnalysisClient
 
@@ -14,8 +16,10 @@ Remember to remove the key from your code when you're done, and never post it pu
 secure methods to store and access your credentials. For more information, see 
 https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-security?tabs=command-line%2Ccsharp#environment-variables-and-application-configuration
 """
-endpoint = "YOUR_FORM_RECOGNIZER_ENDPOINT"
-key = "YOUR_FORM_RECOGNIZER_KEY"
+
+load_dotenv()
+endpoint = os.getenv('AZURE_FORM_RECOGNIZER_ENDPOINT')
+key = os.getenv('AZURE_FORM_RECOGNIZER_KEY')
 
 # sample document
 formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-layout.pdf"
@@ -24,34 +28,25 @@ document_analysis_client = DocumentAnalysisClient(
     endpoint=endpoint, credential=AzureKeyCredential(key)
 )
     
-poller = document_analysis_client.begin_analyze_document_from_url("prebuilt-layout", formUrl)
-result = poller.result()
+# poller = document_analysis_client.begin_analyze_document_from_url("prebuilt-layout", formUrl)
+filepath = "./data/Preferred_Gold_EPO_1500_Benefit_2023_in_Washington.pdf"
+filepath = "C:/Users/hyssh/workspace/openai-demo/quickstart-learnfast/document-intelligent/data/Preferred_Gold_EPO_1500_Benefit_2023_in_Washington.pdf"
+with open(filepath, "rb") as f:
+    poller = document_analysis_client.begin_analyze_document("prebuilt-layout",f )
+    result = poller.result()
 
-for idx, style in enumerate(result.styles):
-    print(
-        "Document contains {} content".format(
-         "handwritten" if style.is_handwritten else "no handwritten"
-        )
-    )
 
-for page in result.pages:
-    for line_idx, line in enumerate(page.lines):
-        print(
-         "...Line # {} has text content '{}'".format(
-        line_idx,
-        line.content.encode("utf-8")
-        )
-    )
-
-    for selection_mark in page.selection_marks:
-        print(
-         "...Selection mark is '{}' and has a confidence of {}".format(
-         selection_mark.state,
-         selection_mark.confidence
-         )
-    )
+# for para_index, paragraph in enumerate(result.paragraphs):
+#     if paragraph.role == "pageNumber":
+#         if hasattr(paragraph, "bounding_regions") and len(paragraph.bounding_regions) > 0:
+#             print(f"Book page {paragraph.content} is found on pdf page {paragraph.bounding_regions[0].page_number}")
+#         else:
+#             print(f"Book page {paragraph.content} is not found on any pdf page")
+#     else:
+#         pass
 
 for table_idx, table in enumerate(result.tables):
+    print("--- Table No. {} ---".format(table_idx + 1))
     print(
         "Table # {} has {} rows and {} columns".format(
         table_idx, table.row_count, table.column_count
@@ -67,5 +62,4 @@ for table_idx, table in enumerate(result.tables):
             )
         )
 
-print("----------------------------------------")
-
+    print("--- Table End ---")
